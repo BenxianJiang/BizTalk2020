c76d94d716774a7e661fba94a5c5db87b8391a77
Ben <benxian.jiang@dxc.com>
2020-08-31 10:47:05 +1000

    Add project files.

---
+++ Ben.Demo.BizTalk.Components/Core.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ben.Demo.BizTalk.Components
+{
+    public class Core
+    {
+        public static string SayWelcome(string firstName, string surName)
+        {
+            return string.Format("Hello, {0} {1}, Welcome!", firstName, surName);
+        }
+    }
+}
---
+++ SolutionFiles/Build_BTDF_BenDemoBizTalk.bat
@@ -0,0 +1,2 @@
+"C:\Windows\Microsoft.NET\Framework\V4.0.30319\MSBuild.exe" /p:Configuration=Debug /t:Installer "C:\GitHub\Ben\Ben.Demo.BizTalk\Ben.Demo.BizTalk.Deployment\Deployment.btdfproj"
+Pause
