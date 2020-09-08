fcb632d0bc3db20ee464b11de261fcd720880c33
Ben <benxian.jiang@dxc.com>
2020-09-08 14:30:21 +1000

    Add Biz Service host in IIS

---
+++ Ben.Demo.UnitTest/BizArticleUploadDownloadTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Ben.Demo.UnitTest
+{
+    [TestClass]
+    public class BizArticleUploadDownloadTest
+    {
+        [TestMethod]
+        public void UploadTestMethod()
+        {
+            string filePath = @"C:\GitHub\Ben\Ben.Demo.BizTalk\Ben.Demo.UnitTest\Data\UploadFile.pdf";
+
+            BizArticleService.BizArticleServiceClient client = new BizArticleService.BizArticleServiceClient("CustomBinding_ITwoWayAsync");
+
+            List<BizArticleService.BizArticlesArticle> arts = new List<BizArticleService.BizArticlesArticle>();
+            BizArticleService.BizArticlesArticle art = new BizArticleService.BizArticlesArticle();
+            art.ArticleName = "article 1";
+            art.ArticleContent = File.ReadAllBytes(filePath);
+            arts.Add(art);
+
+            art.ArticleName = "Article 2";
+            art.ArticleContent = File.ReadAllBytes(filePath);
+            arts.Add(art);
+
+            BizArticleService.BizArticlesArticle[] req = arts.ToArray();
+
+            //for windows authentication
+            client.ClientCredentials.Windows.ClientCredential = System.Net.CredentialCache.DefaultNetworkCredentials;
+            //or use client.ClientCredentials.Windows.ClientCredential = new System.Net.NetworkCredential("userName", "password", "domain");
+
+            client.Upload(ref req);
+
+            foreach(var r  in req)
+            {
+                var name = r.ArticleName;
+                var content = r.ArticleContent;
+            }
+        }
+    }
+}
---
+++ Ben.Demo.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ben.Demo.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ben.Demo.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b50b5615-9ff7-45b3-b029-f2fd8d12ed39")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
