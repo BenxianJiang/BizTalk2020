c5956cb0e55076da3b2c2ed7512e24e3771cbbb4
Ben <benxian.jiang@dxc.com>
2023-05-25 18:24:14 +1000

    Add Dynamic SFTP send

---
+++ Ben.Demo.BizTalk.Deployment/Scripts/Build_BTDF_BenDemoBizTalk.bat
@@ -0,0 +1,3 @@
+REM use the following to build BizTalk deployment MSI. The MSI result will locate in bin folder of deployment project.
+"C:\Windows\Microsoft.NET\Framework\V4.0.30319\MSBuild.exe" /p:Configuration=Debug /t:Installer "C:\GitHub\Ben\BizTalk2020\Ben.Demo.BizTalk.Deployment\Deployment.btdfproj"
+Pause
---
+++ Ben.Demo.BizTalk.Deployment/Scripts/RegisterBenDemoSourceToEventLog.ps1
@@ -0,0 +1,23 @@
+﻿write-Host("Execute this Script Only If This is the FIRST time the code is being deployed on The  environment, if it is a new release DO NOT run this ")
+write-Host("Enter Y to Proceed and N to cancel")
+$flag = Read-Host
+if($flag -eq 'Y')
+{
+	$ExecuionPolicy = Get-ExecutionPolicy 
+write-host("Current Execution Policy Is")
+write-host($ExecuionPolicy )
+write-host('Setting Execution policy to RemoteSigned TO Execute the script')
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned 
+New-EventLog -LogName 'Application' -Source 'BizTalk Ben Demo'
+write-host('Resetting the Execution policy to previous value')
+Set-ExecutionPolicy -ExecutionPolicy $ExecuionPolicy 
+
+$ExecuionPolicyAfterResetting = Get-ExecutionPolicy 
+write-host($ExecuionPolicyAfterResetting )
+
+}
+else
+{
+	write-host("Script Execution Cancelled")
+}
+Read-Host 
